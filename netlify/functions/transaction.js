f3b5c4d0fb809fb16533bf0671ea0e56d240d8e9
pitasker-pi <taskerforpi.app@gmail.com>
2025-07-06 14:24:41 +0100

    Update transaction.js

--- netlify/functions/transaction.js
+++ netlify/functions/transaction.js
@@ -6,8 +6,11 @@ const serviceAccount = {
     "type": process.env.FIREBASE_TYPE,
     "project_id": process.env.FIREBASE_PROJECT_ID,
     "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-    "private_key": process.env.FIREBASE_PRIVATE_KEY, //
-    "client_email": process.env.
+    // *** ANGYARA NAN DON AMFANI DA BASE64 ENCODED PRIVATE KEY ***
+    // Wannan layin zai karbi BASE64_ENCODED_PRIVATE_KEY din daga Netlify
+    // sannan ya maida shi zuwa asalin tsarin private key din
+    "private_key": Buffer.from(process.env.FIREBASE_PRIVATE_KEY_BASE64, 'base64').toString('ascii').replace(/\\n/g, '\n'),
+    "client_email": process.env.FIREBASE_CLIENT_EMAIL, // Na gyara wannan layin domin ya cika
     "client_id": process.env.FIREBASE_CLIENT_ID,
     "auth_uri": process.env.FIREBASE_AUTH_URI,
     "token_uri": process.env.FIREBASE_TOKEN_URI,
